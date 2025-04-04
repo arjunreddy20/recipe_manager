348171772aca5ad18426f9f321261eda8a5156c3
arjunreddy20 <arjunkalvakadi>
2025-04-04 17:19:53 +0530

    db_to_render

--- config/database.js
+++ config/database.js
@@ -1,3 +1,15 @@
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST || '0.0.0.0',
+//     dialect: 'mysql',
+//   }
+// );
+
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
@@ -5,9 +17,12 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || '0.0.0.0',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
+    logging: false,
   }
 );
+
 
 module.exports = sequelize;
